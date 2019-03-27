37c869ef189bca9d044312f028fc54160927a7fb
iamboyoungkim <boyoung3205@gmail.com>
2019-03-27 16:40:42 +0900

    solve 1 problem

--- 14500.cpp
+++ 14500.cpp
@@ -1,101 +1,89 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cstring>
+
 using namespace std;
-int a[500][500];
+
+int dx[4] = {-1,1,0,0};
+int dy[4] = {0,0,-1,1};
+bool check[501][501];
+int arr[501][501];
+int n,m;
+
+int res = -1;
+
+
+void otherCheck() {
+    // ㅗ
+    for(int i = 1; i < n; i ++) {
+        for(int j = 1; j < m - 1; j ++) {
+            int tmp = arr[i][j] + arr[i - 1][j] + arr[i][j - 1] + arr[i][j + 1];
+            if (res < tmp) res = tmp;;
+        }
+    }
+
+    // ㅜ
+    for(int i = 0; i < n - 1; i ++) {
+        for(int j = 1; j < m - 1; j ++) {
+            int tmp = arr[i][j] + arr[i + 1][j] + arr[i][j - 1] + arr[i][j + 1];
+            if (res < tmp) res = tmp;;
+        }
+    }
+
+    // ㅓ
+    for(int i = 1; i < n - 1; i ++) {
+        for(int j = 1; j < m; j ++) {
+            int tmp = arr[i][j] + arr[i + 1][j] + arr[i - 1][j] + arr[i][j - 1];
+            if (res < tmp) res = tmp;;
+        }
+    }
+
+    // ㅏ
+    for(int i = 1; i < n - 1; i ++) {
+        for(int j = 0; j < m - 1; j ++) {
+            int tmp = arr[i][j] + arr[i + 1][j] + arr[i - 1][j] + arr[i][j + 1];
+            if (res < tmp) res = tmp;;
+        }
+    }
+}
+
+void dfs(int posX, int posY, int idx, int sum){
+    if (idx == 4){
+        if (res < sum) res = sum;
+        return;
+    }
+    check[posX][posY] = true;
+    for (int i = 0; i<4; i++){
+        int nextX = posX + dx[i];
+        int nextY = posY + dy[i];
+        if (nextX >= 0 && nextX < n && nextY >=0 && nextY < m && check[nextX][nextY] == false){
+            check[posX][posY] = true;
+            dfs(nextX, nextY, idx+1, sum+arr[nextX][nextY]);
+            check[posX][posY] = false;
+        }
+    }
+}
+
 int main() {
-    int n, m;
     cin >> n >> m;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
-            cin >> a[i][j];
+    for (int i = 0; i<n; i++){
+        for (int j = 0; j<m; j++){
+            cin >> arr[i][j];
         }
     }
-    int ans = 0;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
-            if (j+3 < m) {
-                int temp = a[i][j] + a[i][j+1] + a[i][j+2] + a[i][j+3];
-                if (ans < temp) ans = temp;
-            }
-            if (i+3 < n) {
-                int temp = a[i][j] + a[i+1][j] + a[i+2][j] + a[i+3][j];
-                if (ans < temp) ans = temp;
-            }
-            if (i+1 < n && j+2 < m) {
-                int temp = a[i][j] + a[i+1][j] + a[i+1][j+1] + a[i+1][j+2];
-                if (ans < temp) ans = temp;
-            }
-            if (i+2 < n && j+1 < m) {
-                int temp = a[i][j] + a[i][j+1] + a[i+1][j] + a[i+2][j];
-                if (ans < temp) ans = temp;
-            }
-            if (i+1 < n && j+2 < m) {
-                int temp = a[i][j] + a[i][j+1] + a[i][j+2] + a[i+1][j+2];
-                if (ans < temp) ans = temp;
-            }
-            if (i+2 < n && j-1 >= 0) {
-                int temp = a[i][j] + a[i+1][j] + a[i+2][j] + a[i+2][j-1];
-                if (ans < temp) ans = temp;
-            }
-            if (i-1 >= 0 && j+2 < m) {
-                int temp = a[i][j] + a[i][j+1] + a[i][j+2] + a[i-1][j+2];
-                if (ans < temp) ans = temp;
-            }
-            if (i+2 < n && j+1 < m) {
-                int temp = a[i][j] + a[i+1][j] + a[i+2][j] + a[i+2][j+1];
-                if (ans < temp) ans = temp;
-            }
-            if (i+1 < n && j+2 < m) {
-                int temp = a[i][j] + a[i][j+1] + a[i][j+2] + a[i+1][j];
-                if (ans < temp) ans = temp;
-            }
-            if (i+2 < n && j+1 < m) {
-                int temp = a[i][j] + a[i][j+1] + a[i+1][j+1] + a[i+2][j+1];
-                if (ans < temp) ans = temp;
-            }
-            if (i+1 < n && j+1 < m) {
-                int temp = a[i][j] + a[i][j+1] + a[i+1][j] + a[i+1][j+1];
-                if (ans < temp) ans = temp;
-            }
-            if (i-1 >= 0 && j+2 < m) {
-                int temp = a[i][j] + a[i][j+1] + a[i-1][j+1] + a[i-1][j+2];
-                if (ans < temp) ans = temp;
-            }
-            if (i+2 < n && j+1 < m) {
-                int temp = a[i][j] + a[i+1][j] + a[i+1][j+1] + a[i+2][j+1];
-                if (ans < temp) ans = temp;
-            }
-            if (i+1 < n && j+2 < m) {
-                int temp = a[i][j] + a[i][j+1] + a[i+1][j+1] + a[i+1][j+2];
-                if (ans < temp) ans = temp;
-            }
-            if (i+2 < n && j-1 >= 0) {
-                int temp = a[i][j] + a[i+1][j] + a[i+1][j-1] + a[i+2][j-1];
-                if (ans < temp) ans = temp;
-            }
-            if (j+2 < m) {
-                int temp = a[i][j] + a[i][j+1] + a[i][j+2];
-                if (i-1 >= 0) {
-                    int temp2 = temp + a[i-1][j+1];
-                    if (ans < temp2) ans = temp2;
-                }
-                if (i+1 < n) {
-                    int temp2 = temp + a[i+1][j+1];
-                    if (ans < temp2) ans = temp2;
-                }
-            }
-            if (i+2 < n) {
-                int temp = a[i][j] + a[i+1][j] + a[i+2][j];
-                if (j+1 < m) {
-                    int temp2 = temp + a[i+1][j+1];
-                    if (ans < temp2) ans = temp2;
-                }
-                if (j-1 >= 0) {
-                    int temp2 = temp + a[i+1][j-1];
-                    if (ans < temp2) ans = temp2;
-                }
-            }
+
+    for (int i = 0; i<n; i++){
+        for (int j = 0; j<m; j++){
+            check[i][j] = true;
+            dfs(i,j,1,arr[i][j]);
+            check[i][j] = false;
         }
     }
-    cout << ans << '\n';
+
+    otherCheck();
+    cout << res;
+
     return 0;
 }
